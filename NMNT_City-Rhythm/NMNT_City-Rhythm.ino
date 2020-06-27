b8f89513283abe2b4efaee232bf56e9ae9cbcc47
Olaf Wisselink <olafwisselink@gmail.com>
2020-06-27 14:10:51 +0200

    add dist sensor and servo freq smooth

--- NMNT_City-Rhythm/NMNT_City-Rhythm.ino
+++ NMNT_City-Rhythm/NMNT_City-Rhythm.ino
@@ -1,38 +1,19 @@
 #include <Adafruit_PWMServoDriver.h>
+#include "nmnt_dist_sensor.hpp"
 
 Adafruit_PWMServoDriver servoShield = Adafruit_PWMServoDriver();
 
 // --------Servos-------//
 const int numServo = 1;
-const int PULSEMIN = 125; // Minimum pulse: ~125
-const int PULSEMAX = 225; // Maximum pulse: ~550
+const int PULSEMIN = 200;     // Minimum pulse: ~125
+const int PULSEMAX = 300;     // Maximum pulse: ~550
 
-float freq[numServo]; // Servo speed (Hz)
-float modFreq = 0.05; // Hz
-float modAmp = 0; // amount of modulation (Hz)
+float servoFreq[numServo];     // Servo speed (Hz)
+float targetFreq[numServo];
 
-
-// --------PIR Sensors-------//
-
-// A struct to represent a pair of PIR sensors
-struct PIR {
-  int left;   // left sensor
-  int right;  // right sensor
-
-  boolean prevL = false;
-  boolean prevR = false;
-
-  boolean fromLeft = false;
-  boolean timerStart = false;
-  unsigned long startT = 0;
-  unsigned long interval = 0;
-};
-
-const int numSensorPairs = 1;
-PIR pir[numSensorPairs];
-
-int calibrationTime = 10; // seconds
-
+float minServoFreq = 0.2; // (Hz)
+float maxServoFreq = 4;   // (Hz)
+float freqSmooth = 0.995; // smoothing factor to lerp between freqs (0-1)
 
 void setup() {
   Serial.begin(9600);
@@ -41,99 +22,110 @@ void setup() {
   servoShield.setPWMFreq(60);
 
   for (int i = 0; i < numServo; i++) {
-    freq[i] = 0.5; // Initial frequency
+    servoFreq[i] = 2; // Initial frequency
+    targetFreq[i] = servoFreq[i];
     servoShield.setPWM(i, 0, PULSEMIN);
   }
 
-  int pinOffset = 6;
-  for (int i = 0; i < numSensorPairs; i++) {
-    pir[i].left = i + pinOffset;
-    pir[i].right = i + pinOffset + 1;
-
-    pinMode(pir[i].left, INPUT);
-    pinMode(pir[i].right, INPUT);
-    digitalWrite(pir[i].left, LOW);
-    digitalWrite(pir[i].right, LOW);
-  }
-
-  // give the sensors some time to calibrate
-  Serial.print("calibrating sensor ");
-  for (int i = 0; i < calibrationTime; i++) {
-    Serial.print(".");
-    delay(1000);
-  }
-  Serial.println(" done");
-  Serial.println("SENSOR ACTIVE");
-  delay(50);
+  pinMode(trigPin1, OUTPUT); // Sets the trigPin as an Output
+  pinMode(echoPin1, INPUT); // Sets the echoPin as an Input
+  pinMode(trigPin2, OUTPUT); // Sets the trigPin as an Output
+  pinMode(echoPin2, INPUT); // Sets the echoPin as an Input
 }
 
 
 void loop() {
-  
-  // Read through all the sensors
-  for (int i = 0; i < numSensorPairs; i++) {
-    boolean sensL = digitalRead(pir[i].left);
-    boolean sensR = digitalRead(pir[i].right);
+  float cm1, cm2;                         // Used to save the distance in cm in every frame
+  delayMicroseconds(5);
 
-    // If we haven't detected anything yet
-    if (!pir[i].timerStart) {
-      // If the left sensor detects a state from LOW to HIGH first
-      if ((!pir[i].prevL && sensL) && (!pir[i].prevR && !sensR)) {
-        pir[i].fromLeft = true;
-        pir[i].timerStart = true;
-        pir[i].startT = millis();
-        Serial.println("From left");
-      }
-      
-      // If the right sensor detects a state from LOW to HIGH first
-      else if ((!pir[i].prevR && sensR) && (!pir[i].prevL && !sensL)) {
-        pir[i].fromLeft = false;
-        pir[i].timerStart = true;
-        pir[i].startT = millis();
-        Serial.println("From right");
-      }
+  cm1 = range(trigPin1, echoPin1);        // Calculates the current distance in cm (see range function)
+  delayMicroseconds(10);
+  average1[0] = cm1;                      // The first item of the array is the current distance
+  lastSteps1[0] = cm1;
+
+  cm2 = range(trigPin2, echoPin2);
+  delayMicroseconds(10);
+  average2[0] = cm2;
+  lastSteps2[0] = cm2;
+
+  boolean sensL = walkingBy(lastSteps2, average2);
+  boolean sensR = walkingBy(lastSteps1, average1);
+
+  // If we haven't detected anything yet
+  if (!timerStart) {
+    // If the left sensor detects a state from LOW to HIGH first
+    if (sensL && !sensR && millis() - debounceL > bounceTime) {
+      fromLeft = true;
+      timerStart = true;
+      debounceL = millis();
+      Serial.println("From left");
     }
 
-    // We've detected one sensor and started timing.
-    // See when the other sensor detects.
-    else {
-      // Left sensor was first.
-      // Stop timing when the right sensor is HIGH.
-      if (pir[i].fromLeft) {
-        if (sensR == HIGH) {
-          pir[i].interval = millis() - pir[i].startT;
-          Serial.print("Arrived right at: ");
-          Serial.println(pir[i].interval);
-          pir[i].timerStart = false;
-        }
-      }
-
-      // Right sensor was first
-      // Stop timing when the left sensor is HIGH.
-      else if (sensL == HIGH) {
-        pir[i].interval = millis() - pir[i].startT;
-        Serial.print("Arrived left at: ");
-        Serial.println(pir[i].interval);
-        pir[i].timerStart = false;
-      }
+    // If the right sensor detects a state from LOW to HIGH first
+    else if (sensR && !sensL && millis() - debounceR > bounceTime) {
+      fromLeft = false;
+      timerStart = true;
+      debounceR = millis();
+      Serial.println("From right");
     }
-    
-    pir[i].prevL = sensL;
-    pir[i].prevR = sensR;
   }
 
-  //-----Set the servos------//
-//  float t = millis() * 0.001;
-//  float mod = (sin(TWO_PI * t * modFreq) * 0.5 + 0.5) * modAmp;
-//
-//  for (int i = 0; i < numServo; i++) {
-//    float phi = sin((TWO_PI * t * freq[i]) + mod);
-//
-//    int PWM = (int)floatMap(phi, -1.0, 1.0, PULSEMIN, PULSEMAX);
-//    servoShield.setPWM(i, 0, PWM);
-//  }
+  // We've detected one sensor and started timing.
+  // See when the other sensor detects.
+  else {
+    // Left sensor was first.
+    // Stop timing when the right sensor is HIGH.
+    if (fromLeft) {
+      if (sensR == HIGH) {
+        // left debounce time is interval
+        sensorInterval = millis() - debounceL;
+        Serial.print("Arrived right at: ");
+        Serial.println(sensorInterval);
+        timerStart = false;
+        debounceR = millis();
+      }
+    }
+
+    // Right sensor was first
+    // Stop timing when the left sensor is HIGH.
+    else if (sensL == HIGH) {
+      // right debounce time is interval
+      sensorInterval = millis() - debounceR;
+      Serial.print("Arrived left at: ");
+      Serial.println(sensorInterval);
+      timerStart = false;
+      debounceL = millis();
+    }
+  }
+
+  //-----Servos------//
+
+  // Set the servo speed corresponding to sonar interval
+  float sonarFreq = 1000.0 / (float)sensorInterval;
+  
+  if (sonarFreq > minServoFreq && sonarFreq < maxServoFreq) {
+    for (int i = 0; i < numServo; i++) {
+      targetFreq[i] = sonarFreq;
+      freqSmooth = floatMap(abs(servoFreq[i] - targetFreq[i]), 0, maxServoFreq-minServoFreq, 0.75, 0.99);
+    }
+  }
+
+  float t = millis() * 0.001; // time (s)
+
+  for (int i = 0; i < numServo; i++) {
+    servoFreq[i] = lerp(servoFreq[i], targetFreq[i], freqSmooth);
+
+    float phi = sin(TWO_PI * t * servoFreq[i]);
+
+    int PWM = (int)floatMap(phi, -1.0, 1.0, PULSEMIN, PULSEMAX);
+    servoShield.setPWM(i, 0, PWM);
+  }
 }
 
 float floatMap(double x, double in_min, double in_max, double out_min, double out_max) {
   return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
+
+float lerp(float x, float y, float m) {
+  return m * x + (1.0 - m) * y;
 }
---
+++ NMNT_City-Rhythm/nmnt_dist_sensor.hpp
@@ -0,0 +1,104 @@
+#ifndef _NMNT_DIST_SENSOR_HPP_
+#define _NMNT_DIST_SENSOR_HPP_
+
+#include "Arduino.h"
+//#include <NewPing.h>
+
+// defines pins numbers
+const int trigPin1 = 9;       // the pin trig should be connected to 9
+const int echoPin1 = 10;      // the pin echo should be connected to 10
+const int trigPin2 = 5;
+const int echoPin2 = 6;
+
+// defines variables for distance measurement
+long duration1;
+int distance1;
+long duration2;
+long distance2;
+
+// defines arrays to measure the average distance over time (used for calibration)
+const int numAvg = 100;
+const int numStep = 3;
+unsigned long average1[numAvg];
+unsigned long average2[numAvg];
+unsigned long lastSteps1[numStep];
+unsigned long lastSteps2[numStep];
+//long margin1;
+//long margin2;
+
+// Timing between sensors
+boolean fromLeft = false;
+boolean timerStart = false;
+unsigned long sensorInterval = 0;
+
+unsigned long debounceL = 0;
+unsigned long debounceR = 0;
+unsigned int bounceTime = 2000; // (ms)
+
+unsigned long range (byte trig, byte echo) {          // Calculates the distance in cm
+
+  digitalWrite(trig, LOW);
+  digitalWrite(trig, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trig, LOW);
+  unsigned long duration = pulseIn(echo, HIGH);
+  return duration / 58.2;
+
+}
+
+unsigned long calculateAverage(long input[], int arraySize) {   // Calculates the average of the array that is used for calibraton
+
+  long sum = 0;
+  long average;
+
+  for (int i = arraySize - 1; i > 0; i--) {
+    input[i] = input[i - 1];
+  }
+
+  for (int j = 0; j < arraySize; j++) {
+    sum += input[j];
+  }
+
+  average = sum / arraySize;
+
+  return average;
+
+}
+//
+//unsigned long lastSteps(long input[]) {         // Calculates the average of the last three distances (could also have used the previous function, but C does not have an array.length() function and mweh)
+//
+//  long sum = 0;                                 // The reason for this function is that the sensor is sometimes faulty, but it's always only one number that's faulty.
+//  long average;
+//
+//  for (int i = 2; i > 0; i--) {
+//    input[i] = input[i - 1];
+//  }
+//
+//  for (int j = 0; j < 3; j++) {
+//    sum += input[j];
+//  }
+//
+//  average = sum / 3;
+//
+//  return average;
+//
+//}
+
+unsigned long walkingBy(long stepping[], long input[]) {
+  long margin;
+
+  long currentDist = calculateAverage(stepping, numStep);           // Calculates the average of the last three distances
+  bool someoneWalksBySensor;
+                                                                    // Calculates the average distance in the last 100 frames
+  margin = calculateAverage(input, numAvg) * 0.75;                  // Someone is walking by if they're at least 25% closer to the server than the average distance
+
+  if (currentDist < margin) {                                       // Checks if someone is walking by
+    someoneWalksBySensor = true;
+  }
+  else {
+    someoneWalksBySensor = false;
+  }
+  return someoneWalksBySensor;
+}
+
+#endif
